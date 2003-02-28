d40dbd6d31b6420ee1b8744341969b16f51b4d89
Wojciech Polak <polak@gnu.org>
2003-02-28 20:42:58 +0000

    Added missing copyright.

--- src/list.h
+++ src/list.h
@@ -1,4 +1,27 @@
-/* list.c */
+/*
+   list.h
+
+   This file is part of GNU Anubis.
+   Copyright (C) 2003 The Anubis Team.
+
+   GNU Anubis is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   GNU Anubis is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU Anubis; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   GNU Anubis is released under the GPL with the additional exemption that
+   compiling, linking, and/or using OpenSSL is allowed.
+*/
+
 struct list;
 
 typedef int (*list_iterator_t)(void *item, void *data);
@@ -17,3 +40,6 @@ void *list_locate(struct list *list, void *data, list_comp_t cmp);
 void *list_remove_current(struct list *list);
 void *list_remove(struct list *list, void *data, list_comp_t cmp);
 void list_append_list(struct list *a, struct list *b);
+
+/* EOF */
+
