8f8ceb02f49be6e8d17249f2670abb07b5838a5a
Sergey Poznyakoff <gray@gnu.org.ua>
2003-08-24 16:13:28 +0000

    (sig_local): Changed return type. Removed code. (cleanup_children): New function.

--- src/exec.c
+++ src/exec.c
@@ -124,8 +124,8 @@ make_sockets(int fd[2])
 	return 0;
 }
 
-static void
-sig_local(int code)
+void
+cleanup_children()
 {
 	pid_t pid;
 	int status;
@@ -133,7 +133,20 @@ sig_local(int code)
 	while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
 		info(VERBOSE, _("Local program [%lu] finished."),
 		     (unsigned long) pid);
-	return;
+}
+
+static RETSIGTYPE
+sig_local(int code)
+{
+	/* EMPTY FUNCTION */
+	/* Notice: This signal handler is installed only by
+	   make_local_connection, that is used to launch the local
+	   mailer. After the mailer exits, the main code will read
+	   the return code from the mailer's outgoing pipe. If
+	   waitpid() is called before this happens, the pipe will be
+	   broken and the read will fail. Therefore, do not call waitpid()
+	   from the handler. This should be done after smtp_session()
+	   by calling cleanup_children() */
 }
 
 int
