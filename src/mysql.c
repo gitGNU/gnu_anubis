ad8eccd7e134625fe8432ddd3b11b2de9e6be0a0
Sergey Poznyakoff <gray@gnu.org.ua>
2003-11-30 00:47:16 +0000

    New file.

---
+++ src/mysql.c
@@ -0,0 +1,211 @@
+/*
+   mysql.c
+
+   This file is part of GNU Anubis.
+   Copyright (C) 2003 The Anubis Team.
+
+   GNU Anubis is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   GNU Anubis is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU Anubis; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   GNU Anubis is released under the GPL with the additional exemption that
+   compiling, linking, and/or using OpenSSL is allowed.
+*/
+
+#include "headers.h"
+#include "extern.h"
+#if defined(WITH_MYSQL)
+#include <mysql/mysql.h>
+
+/* MySQL URL:
+
+   mysql://user:password@host/dbname;
+          table=STRING;authid=STRING;passwd=STRING[;user=STRING][;rccol=STRING]
+          [;port=NUMBER][;socket=STRING][;bufsize=NUMBER]
+
+   mysql://anubis:seCReT@localhost/ANUBIS;table=users;usercol=user;\
+           passcol=pass;pathcol=rcfile
+*/
+
+struct anubis_mysql_db {
+	MYSQL mysql;
+	char *table;
+	char *authid;
+	char *passwd;
+	char *user;
+	char *rccol;
+	char *buf;
+	size_t bufsize;
+};
+
+/* Open the plaintext database. ARG is the full pathname to the file */
+static int
+mysql_db_open (void **dp, ANUBIS_URL *url, enum anubis_db_mode mode)
+{
+	struct anubis_mysql_db *amp = NULL;
+	const char *table = anubis_url_get_arg(url, "table");
+	const char *authid = anubis_url_get_arg(url, "authid");
+	const char *passwd = anubis_url_get_arg(url, "passwd");
+	const char *user = anubis_url_get_arg(url, "user");
+	const char *rccol = anubis_url_get_arg(url, "rccol");
+	const char *portstr = anubis_url_get_arg(url, "port");
+	const char *s = anubis_url_get_arg(url, "bufsize");
+	int port = 0;
+	size_t bufsize = 1024;
+	
+	if (s) {
+		char *p;
+		bufsize = strtoul(s, &p, 10);
+	}
+		
+	if (portstr) {
+		char *p;
+		port = strtoul(portstr, &p, 10);
+		if (*p)
+			return EINVAL;
+	}
+
+	amp = xmalloc(sizeof(*amp));
+	amp->buf = xmalloc(bufsize);
+	amp->bufsize = bufsize;
+        mysql_init(&amp->mysql);
+	if (!mysql_real_connect(&amp->mysql, 
+				url->host, url->user, url->passwd,
+				url->path, port,
+				anubis_url_get_arg(url, "socket"),
+				0)) {
+		free(amp);
+		return EINVAL;
+	}
+	amp->table = strdup(table);
+	amp->authid = strdup(authid);
+	amp->passwd = strdup(passwd);
+	amp->user = strdup(user);
+	amp->rccol = strdup(rccol);
+
+	*dp = amp;
+	return ANUBIS_DB_SUCCESS;
+}
+
+static int
+mysql_db_close (void *d)
+{
+	struct anubis_mysql_db *amp = d;
+	mysql_close(&amp->mysql);
+	free(amp->table);
+	free(amp->authid);
+	free(amp->passwd);
+	free(amp->user);
+	free(amp->rccol);
+	free(amp->buf);
+	return ANUBIS_DB_SUCCESS;
+}
+
+static int
+mysql_db_get (void *d, char *key, ANUBIS_USER *rec, int *errp)
+{
+        MYSQL_RES *result;
+        MYSQL_ROW row;
+	struct anubis_mysql_db *amp = d;
+
+	snprintf(amp->buf, amp->bufsize,
+		 "SELECT %s,%s,%s,%s FROM %s WHERE %s='%s'",
+		 amp->authid,
+		 amp->passwd,
+		 amp->user,
+		 amp->rccol,
+		 amp->table,
+		 amp->authid,
+		 key);
+
+	*errp = mysql_query(&amp->mysql, amp->buf);
+	if (*errp)
+		return ANUBIS_DB_FAIL;
+        if (!(result = mysql_store_result(&amp->mysql)))
+		return ANUBIS_DB_FAIL;
+        if (mysql_num_rows(result) == 0) {
+		mysql_free_result(result);
+		return ANUBIS_DB_NOT_FOUND;
+	}
+
+	row = mysql_fetch_row(result);
+	
+	rec->smtp_authid = row[0];
+	rec->smtp_passwd = row[1];
+	if (row[2])
+		rec->username = row[2];
+	if (row[3])
+		rec->rc_file_name = row[3];
+	mysql_free_result(result);
+	return ANUBIS_DB_SUCCESS;
+}
+
+#define MSTR(s) ((s) ? (s) : "NULL")
+
+static int
+mysql_db_put (void *d, char *key, ANUBIS_USER *rec, int *errp)
+{
+	struct anubis_mysql_db *amp = d;
+
+	snprintf(amp->buf, amp->bufsize,
+		 "REPLACE INTO %s (%s,%s,%s,%s) VALUES (%s,%s,%s,%s)",
+		 amp->table,
+		 amp->authid,
+		 amp->passwd,
+		 amp->user,
+		 amp->rccol,
+		 rec->smtp_authid,
+		 rec->smtp_passwd,
+		 MSTR(rec->username),
+		 MSTR(rec->rc_file_name));
+	*errp = mysql_query(&amp->mysql, amp->buf);
+	if (*errp)
+		return ANUBIS_DB_FAIL;
+	return ANUBIS_DB_SUCCESS;
+}
+
+static int
+mysql_db_delete(void *d, char *keystr, int *ecode)
+{
+	struct anubis_mysql_db *amp = d;
+
+	snprintf(amp->buf, amp->bufsize,
+		 "DELETE FROM %s WHERE %s='%s'",
+		 amp->table,
+		 amp->authid,
+		 keystr);
+	*ecode = mysql_query(&amp->mysql, amp->buf);
+	if (*ecode)
+		return ANUBIS_DB_FAIL;
+	return ANUBIS_DB_SUCCESS;
+}
+
+const char *
+mysql_db_strerror(void *d, int rc)
+{
+	struct anubis_mysql_db *amp = d;
+	return mysql_error(&amp->mysql);
+}
+
+void
+mysql_db_init ()
+{
+	anubis_db_register("mysql",
+			   mysql_db_open,
+			   mysql_db_close,
+			   mysql_db_get,
+			   mysql_db_put,
+			   mysql_db_delete,
+			   mysql_db_strerror);
+}
+#endif
---
+++ src/url.c
@@ -0,0 +1,243 @@
+/*
+   url.c
+
+   This file is part of GNU Anubis.
+   Copyright (C) 2003 The Anubis Team.
+
+   GNU Anubis is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   GNU Anubis is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GNU Anubis; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+   GNU Anubis is released under the GPL with the additional exemption that
+   compiling, linking, and/or using OpenSSL is allowed.
+*/
+
+#include "headers.h"
+#include "extern.h"
+#include <assert.h>
+
+/* mech://[user[:password]@][host/]path[;arg=str[;arg=str...] */
+
+static void
+alloc_string(char **sptr, char *start, char *end)
+{
+	size_t len = end - start;
+	*sptr = xmalloc(len+1);
+	memcpy(*sptr, start, len);
+	(*sptr)[len] = 0;
+}
+
+static void
+url_parse_arg(ASSOC *assoc, char *p, char *q)
+{
+	char *s;
+
+	for (s = p; s < q && *s != '='; s++)
+		;
+
+	alloc_string(&assoc->key, p, s);
+	if (s != q)
+		alloc_string(&assoc->value, s + 1, q);
+}
+
+static int
+url_get_args(ANUBIS_URL *url, char **str)
+{
+	char *p;
+	int argc;
+
+	if (!**str)
+		return 0;
+	
+	for (p = *str, argc = 1; *p; p++)
+		if (*p == ';')
+			argc++;
+
+	url->argc = argc;
+	url->argv = calloc(argc, sizeof(url->argv[0]));
+	assert(url->argv != NULL);
+
+	for (argc = 0, p = *str; *p; argc++) {
+		char *q = strchr(p, ';');
+		if (q) {
+			url_parse_arg(&url->argv[argc], p, q);
+			p = q + 1;
+		} else {
+			q = p + strlen(p);
+			url_parse_arg(&url->argv[argc], p, q);
+			p = q;
+		}
+	}
+	return 0;
+}
+
+static int
+url_get_path(ANUBIS_URL *url, char **str)
+{
+	char *p;
+
+	p = strchr(*str, ';');
+	if (!p)
+		p = *str + strlen(*str);
+	alloc_string(&url->path, *str, p);
+	*str = p;
+	if (*p)
+		++*str;
+	return url_get_args(url, str);
+
+}
+
+/* On input str points at the beginning of host part */
+static int
+url_get_host(ANUBIS_URL *url, char **str)
+{
+	char *p;
+
+	p = strchr(*str, '/');
+	
+	if (p) {
+		alloc_string(&url->host, *str, p);
+		*str = p + 1;
+	}
+	return url_get_path(url, str);
+}
+
+/* On input str points past the ':' */
+static int
+url_get_passwd(ANUBIS_URL *url, char **str)
+{
+	char *p;
+
+	p = strchr(*str, '@');
+	
+	if (p) {
+		alloc_string(&url->passwd, *str, p);
+		*str = p + 1;
+	}
+	return url_get_host(url, str);
+}
+
+/* On input str points past the mech:// part */
+static int
+url_get_user(ANUBIS_URL *url, char **str)
+{
+	char *p;
+
+	for (p = *str; *p && !strchr(":@", *p); p++)
+		;
+	
+	switch (*p) {
+	case ':':
+		alloc_string(&url->user, *str, p);
+		*str = p + 1;
+		return url_get_passwd(url, str);
+	case '@':
+		alloc_string(&url->user, *str, p);
+		url->passwd = NULL;
+		*str = p + 1;
+	}
+	return url_get_host(url, str);
+}
+
+static int
+url_get_mech(ANUBIS_URL *url, char *str)
+{
+	char *p;
+
+	p = strchr(str, ':');
+	if (!p)
+		return 1;
+	alloc_string(&url->method, str, p);
+
+	/* Skip slashes */
+	for (p++; *p == '/'; p++)
+		;
+	return url_get_user(url, &p);
+}
+
+void
+anubis_url_destroy(ANUBIS_URL **url)
+{
+	int i;
+	
+	free((*url)->method);
+	free((*url)->host);
+	free((*url)->path);
+	free((*url)->user);
+	free((*url)->passwd);
+	for (i = 0; i < (*url)->argc; i++) {
+		free((*url)->argv[i].key);
+		free((*url)->argv[i].value);
+	}
+	free((*url)->argv);
+	free(*url);
+	*url = NULL;
+}
+
+int
+anubis_url_parse(ANUBIS_URL **url, char *str)
+{
+	int rc;
+	
+	*url = xmalloc(sizeof(**url));
+	memset(*url, 0, sizeof(**url));
+	rc = url_get_mech(*url, str);
+	if (rc)
+		anubis_url_destroy(url);
+	return rc;
+}
+
+char *
+anubis_url_full_path(ANUBIS_URL *url)
+{
+	char *path;
+	size_t size = 1 + strlen(url->host);
+	if (url->path)
+		size += strlen(url->path) + 1;
+	path = xmalloc(size + 1);
+	strcpy(path, "/");
+	strcat(path, url->host);
+	if (url->path) {
+		strcat(path, "/");
+		strcat(path, url->path);
+	}
+	return path;
+}
+
+const char *
+anubis_url_get_arg(ANUBIS_URL *url, const char *argname)
+{
+	int i;
+	for (i = 0; i < url->argc; i++) {
+		if (strcasecmp(url->argv[i].key, argname) == 0)
+			return url->argv[i].value;
+	}
+	return NULL;
+}
+
+check_url(char *str)
+{
+	ANUBIS_URL *url;
+
+	int i, rc = anubis_url_parse(&url, str);
+	printf("METHOD: %s\n", url->method);
+	printf("HOST: %s\n", url->host);
+	printf("PATH: %s\n", url->path);
+	printf("USER: %s\n", url->user);
+	printf("PASS: %s\n", url->passwd);
+	printf("ARGC: %d\n", url->argc);
+	for (i = 0; i < url->argc; i++) {
+		printf("%s = %s\n", url->argv[i].key, url->argv[i].value);
+	}
+	return 0;
+}
