3f90833d985983b12bf0499afc9430b30e8fe898
Sergey Poznyakoff <gray@gnu.org.ua>
2004-03-05 23:32:02 +0000

    Split off server-specific functions to a separate module

--- src/gsasl.c
+++ src/gsasl.c
@@ -1,8 +1,8 @@
 /*
-   authmode.c
+   gsasl.c
 
    This file is part of GNU Anubis.
-   Copyright (C) 2003 The Anubis Team.
+   Copyright (C) 2003,2004 The Anubis Team.
 
    GNU Anubis is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -30,20 +30,17 @@
 #include "lbuf.h"
 #include <gsasl.h>
 
-static Gsasl_ctx *ctx;   
-static LIST *anubis_mech_list;
-
 
 /* Basic I/O Functions */
 
 struct anubis_gsasl_stream {
 	Gsasl_session_ctx *sess_ctx; /* Context */
 	struct _line_buffer *lb;
-	void *net_io;
+	NET_STREAM stream;
 };
 
 static const char *
-_gsasl_strerror(int rc)
+_gsasl_strerror(void *ignored_data, int rc)
 {
 	return gsasl_strerror(rc);
 }
@@ -68,7 +65,7 @@ write_chunk (void *data, char *start, char *end)
 	wrsize = 0;
 	do {
 		size_t sz;
-		int rc = net_io_write(s->net_io, buf + wrsize, len - wrsize,
+		int rc = stream_write(s->stream, buf + wrsize, len - wrsize,
 				      &sz);
 		if (rc) {
 			if (rc == EINTR)
@@ -79,7 +76,7 @@ write_chunk (void *data, char *start, char *end)
 		wrsize += sz;
 	}
 	while (wrsize < len);
-  
+	
 	free (buf);
 	
 	return 0;
@@ -110,7 +107,7 @@ _gsasl_read(void *sd, char *data, size_t size, size_t *nbytes)
 		char buf[80];
 		size_t sz;
 		
-		rc = net_io_read(s->net_io, buf, sizeof (buf), &sz);
+		rc = stream_read(s->stream, buf, sizeof (buf), &sz);
 		if (rc) {
 			if (rc == EINTR)
 				continue;
@@ -138,12 +135,12 @@ _gsasl_read(void *sd, char *data, size_t size, size_t *nbytes)
 			   _auth_lb_data (s->lb),
 			   _auth_lb_level (s->lb),
 			   bufp, &len);
-
+	
 	if (rc != GSASL_OK) {
 		free(bufp);
 		return rc;
 	}
-
+	
 	if (len > size) {
 		memcpy(data, bufp, size);
 		_auth_lb_drop (s->lb);
@@ -165,319 +162,34 @@ _gsasl_close(void *sd)
 {
 	struct anubis_gsasl_stream *s = sd;
 
-	net_io_close(s->net_io);
-	if (s->sess_ctx)
-		gsasl_server_finish (s->sess_ctx);
-	_auth_lb_destroy (&s->lb);
+	stream_close(s->stream);
 	return 0;
 }
 
-static void
-install_gsasl_stream (Gsasl_session_ctx *sess_ctx)
+static int
+_gsasl_destroy(void *sd)
+{
+	struct anubis_gsasl_stream *s = sd;
+	if (s->sess_ctx)
+		gsasl_server_finish(s->sess_ctx);
+	_auth_lb_destroy(&s->lb);
+	free(sd);
+	return 0;
+}
+
+void
+install_gsasl_stream (Gsasl_session_ctx *sess_ctx, NET_STREAM *stream)
 {
 	struct anubis_gsasl_stream *s = malloc(sizeof *s);
 
 	s->sess_ctx = sess_ctx;
 	_auth_lb_create (&s->lb);
+	s->stream = *stream;
 
-	s->net_io = net_io_get(SERVER, remote_client);
-	net_set_io(SERVER, _gsasl_read, _gsasl_write,
-		   _gsasl_close, _gsasl_strerror);
-		   remote_client = s;
-}
-
-
-/* GSASL Authentication */
-
-int
-anubis_auth_gsasl (char *auth_type, char *arg, ANUBIS_USER *usr)
-{
-	char *input = arg;
-	size_t input_size = 0;
-	char *output;
-	int rc;
-	Gsasl_session_ctx *sess_ctx = NULL; 
-
-	info(DEBUG, "mech=%s, inp=%s", auth_type, arg);
-
-	memset(usr, 0, sizeof(*usr));
-	rc = gsasl_server_start (ctx, auth_type, &sess_ctx);
-	if (rc != GSASL_OK) {
-		info(NORMAL, _("SASL gsasl_server_start: %s"),
-		     gsasl_strerror(rc));
-		asmtp_reply(504, "%s", gsasl_strerror(rc));
-		return 1;
-	}
-
-	gsasl_server_application_data_set (sess_ctx, usr);
-	
-	output = NULL;
-	/* RFC 2554 4.:
-	    Unlike a zero-length client answer to a 334 reply, a zero-
-            length initial response is sent as a single equals sign */
-	if (input && strcmp(input, "=") == 0)
-		input = "";
-
-	while ((rc = gsasl_step64 (sess_ctx, input, &output))
-	       == GSASL_NEEDS_MORE) {
-		asmtp_reply (334, "%s", output);
-		recvline_ptr(SERVER, remote_client, &input, &input_size);
-		remcrlf(input);
-		if (strcmp (input, "*") == 0) {
-			asmtp_reply (501, "AUTH aborted");
-			return 1;
-		}
-	}
-  
-	if (input_size)
-		free (input);
-	
-	if (rc != GSASL_OK) {
-		info (NORMAL,
-		      _("GSASL error: %s"), gsasl_strerror (rc));
-		free (output);
-		asmtp_reply(501, "Authentication failed");
-		return 1;
-	}
-
-	/* Some SASL mechanisms output data when GSASL_OK is returned */
-	if (output[0])
-		asmtp_reply (334, "%s", output);
-  
-	free (output);
-	     
-	if (usr->smtp_authid == NULL) {
-		info (NORMAL,
-		      _("GSASL %s: cannot get username"), auth_type);
-		asmtp_reply(535, "Authentication failed"); /* FIXME */
-		return 1;
-	}
-
-	info(NORMAL, "Authentication passed. User name %s, Local user %s",
-	     usr->smtp_authid,
-	     usr->username ? usr->username : "NONE");
-
-	if (sess_ctx) 
-		install_gsasl_stream (sess_ctx);
-
-	asmtp_reply (235, "Authentication successful.");
-	return 0;
-}
-
-
-
-/* Converts the auth method list from a textual representation to
-   a LIST of string values */
-static LIST *
-auth_method_list(char *input)
-{
-	char *p;
-	LIST *list = list_create();
-	
-	for (p = strtok(input, " \t"); p; p = strtok(NULL, " \t"))
-		list_append(list, strdup(p));
-	return list;
-}
-
-/* Converts the authentication method LIST to its textual representation. */
-static void
-auth_list_to_string(LIST *list, char *buf, size_t bufsize)
-{
-	ITERATOR *itr = iterator_create(list);
-	char *p;
-	
-	if (!itr)
-		return;
-	for (p = iterator_first(itr); p; p = iterator_next(itr)) {
-		size_t len = strlen(p);
-		if (len + 1 >= bufsize)
-			break;
-		strcpy(buf, p);
-		buf += len;
-		*buf++ = ' ';
-		bufsize -= len + 1;
-	}
-	iterator_destroy(&itr);
-	*buf = 0;
-}
-
-/* Sets the list of allowed authentication mechanisms from its
-   argument */
-void
-anubis_set_mech_list(LIST *list)
-{
-	ITERATOR *itr = iterator_create(list);
-	char *p;
-
-	if (!itr)
-		return;
-	anubis_mech_list = list_create();
-	for (p = iterator_first(itr); p; p = iterator_next(itr)) 
-		list_append(anubis_mech_list, strdup(p));
-	iterator_destroy(&itr);
-}
-
-/* Capability list handling */
-
-static int
-name_cmp(void *item, void *data)
-{
-	return strcmp(item, data);
-}
-
-static void
-auth_gsasl_capa_init ()
-{
-	int rc;
-	char *listmech;
-
-	rc = gsasl_server_mechlist(ctx, &listmech);
-	if (rc != GSASL_OK) {
-		anubis_error(HARD, "%s", gsasl_strerror(rc));
-		return;
-	}
-
-	if (anubis_mech_list) {
-		size_t size = strlen(listmech);
-		LIST *mech = auth_method_list(listmech);
-		LIST *p = list_intersect(mech, anubis_mech_list, name_cmp);
-		auth_list_to_string(p, listmech, size);
-		list_destroy(&p, NULL, NULL);
-		list_destroy(&mech, anubis_free_list_item, NULL);
-	}
-	if (listmech[0])
-		asmtp_capa_add_prefix("AUTH", listmech);
-      
-	free(listmech);
-}
-
-
-/* Various callback functions */
-
-/* This is for DIGEST-MD5 */
-static int
-cb_realm (Gsasl_session_ctx *ctx, char *out, size_t *outlen, size_t nth)
-{
-	char *realm = get_localname ();
-
-	if (nth > 0)
-		return GSASL_NO_MORE_REALMS;
-
-	if (out) {
-		if (*outlen < strlen (realm))
-			return GSASL_TOO_SMALL_BUFFER;
-		memcpy (out, realm, strlen (realm));
-	}
-
-	*outlen = strlen (realm);
-	
-	return GSASL_OK;
-}
-
-static int
-cb_validate (Gsasl_session_ctx *ctx,
-	     const char *authorization_id,
-	     const char *authentication_id,
-	     const char *password)
-{
-	ANUBIS_USER *usr = gsasl_server_application_data_get (ctx);
-
-	if (usr->smtp_authid == NULL
-	    && anubis_get_db_record(authentication_id, usr)
-	    != ANUBIS_DB_SUCCESS)
-		return GSASL_AUTHENTICATION_ERROR;
-	
-	if (usr->smtp_authid == NULL
-	    || strcmp (usr->smtp_authid, authentication_id)
-	    || strcmp (usr->smtp_passwd, password))
-		return GSASL_AUTHENTICATION_ERROR;
-	return GSASL_OK;
-}
-
-#define GSSAPI_SERVICE "anubis"
-
-static int
-cb_service (Gsasl_session_ctx *ctx, char *srv, size_t *srvlen,
-	    char *host, size_t *hostlen)
-{
-	char *hostname = get_localname ();
-
-	if (srv) {
-		if (*srvlen < strlen (GSSAPI_SERVICE))
-			return GSASL_TOO_SMALL_BUFFER;
-		
-		memcpy (srv, GSSAPI_SERVICE, strlen (GSSAPI_SERVICE));
-	}
-
-	if (srvlen)
-		*srvlen = strlen (GSSAPI_SERVICE);
-
-	if (host) {
-		if (*hostlen < strlen (hostname))
-			return GSASL_TOO_SMALL_BUFFER;
-		
-		memcpy (host, hostname, strlen (hostname));
-	}
-	
-	if (hostlen)
-		*hostlen = strlen (hostname);
-	
-	return GSASL_OK;
-}
-
-/* This gets called when SASL mechanism EXTERNAL is invoked */
-static int
-cb_external (Gsasl_session_ctx *ctx)
-{
-	return GSASL_AUTHENTICATION_ERROR;
-}
-
-/* This gets called when SASL mechanism CRAM-MD5 or DIGEST-MD5 is invoked */
-
-static int
-cb_retrieve (Gsasl_session_ctx *ctx,
-	     const char *authentication_id,
-	     const char *authorization_id,
-	     const char *realm,
-	     char *key,
-	     size_t *keylen)
-{
-	ANUBIS_USER *usr = gsasl_server_application_data_get (ctx);
-
-	if (usr->smtp_authid == NULL
-	    && anubis_get_db_record(authentication_id, usr)
-	    != ANUBIS_DB_SUCCESS)
-		return GSASL_AUTHENTICATION_ERROR;
-
-	if (key) {
-		if (*keylen < strlen(usr->smtp_passwd))
-			return GSASL_TOO_SMALL_BUFFER;
-		strncpy(key, usr->smtp_passwd, *keylen);
-	} else
-		*keylen = strlen(usr->smtp_passwd);
-	return GSASL_OK;
-}
-	
-
-/* Initialization function */
-void
-auth_gsasl_init ()
-{
-  int rc;
-  
-  rc = gsasl_init (&ctx);
-  if (rc != GSASL_OK) {
-	  info (NORMAL, _("cannot initialize libgsasl: %s"),
-		gsasl_strerror (rc));
-  }
-
-  gsasl_server_callback_realm_set (ctx, cb_realm);
-  gsasl_server_callback_external_set (ctx, cb_external);
-  gsasl_server_callback_validate_set (ctx, cb_validate);
-  gsasl_server_callback_service_set (ctx, cb_service);
-  gsasl_server_callback_retrieve_set (ctx, cb_retrieve);
-	  
-  auth_gsasl_capa_init ();
+	stream_create(stream);
+	stream_set_io(*stream, s, 
+		      _gsasl_read, _gsasl_write,
+		      _gsasl_close, _gsasl_destroy, _gsasl_strerror);
 }
 
 #endif
