703fc4e013a5e8232ec388fdd41d9eadc15598e2
Wojciech Polak <polak@gnu.org>
2004-07-11 11:15:52 +0000

    Bump to 3.9.95

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*
-   GNU Anubis v3.9.94 -- an outgoing mail processor and the SMTP tunnel.
+   GNU Anubis v3.9.95 -- an outgoing mail processor and the SMTP tunnel.
    Copyright (C) 2001, 2002, 2003, 2004 The Anubis Team.
 
    GNU Anubis is free software; you can redistribute it and/or modify
